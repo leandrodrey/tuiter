4cd585fa33d1fd213dae64ce3c45a44b1570f6ab
Leandro Rey <leandrodrey@gmail.com>
2025-05-21 17:02:23 -0300

    Add layout and login component to HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,18 @@
 import type {JSX} from "react";
+import LoginComponent from "../components/Login/LoginComponent";
+import "./HomePage.css";
 
 const HomePage = (): JSX.Element => {
     return (
-        <>
-            Hola
-        </>
+        <div className="home-container">
+            <div className="login-wrapper">
+                <LoginComponent />
+            </div>
+            <div className="content">
+                <h1>Bienvenido a Tuiter</h1>
+                <p>Esta es la página de inicio de nuestra aplicación.</p>
+            </div>
+        </div>
     )
 }
 
